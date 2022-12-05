dced98cf36d526657132d6f44e488960bf98d24a
Zoltán Nagy <zoltan0803@gmail.com>
2022-12-05 10:03:26 +0100

    refactor: add getlines

---
+++ AdventOfCode/Utils/StringUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Utils
+{
+    public static class StringUtils
+    {
+        public static List<string> GetLines(this string input, bool removeEmptyLines = true)
+        {
+            var o = removeEmptyLines ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None;
+            return (input.Contains('\r') ?
+                        input.Split("\r\n", o) :
+                        input.Split(new char[] { '\r', '\n' }, o))
+                    .ToList();
+
+        }
+    }
+}
